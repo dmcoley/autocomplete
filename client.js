70dfcd2f3756cca1f18c6faefb4f576240d5da18
dmcoley <dmcoley@uw.edu>
2016-09-11 13:15:14 -0700

    initial commit

---
+++ client.js
@@ -0,0 +1,62 @@
+$.getScript('trie.js', function()
+{
+  function $$(id) {
+    return document.getElementById(id);
+  };
+
+  var trie = new Trie();
+  var wordRanks = [];
+  var wiktionary = [];
+
+  function readText(){
+      jQuery.ajax({
+          url : "wiktionary.txt",
+          dataType: "text",
+          success : function (data) {
+              wiktionary = data.split('\n');
+              for (var i = 0; i < wiktionary.length; i++) {
+                var line = wiktionary[i].split('\t');
+                var word = line[1];
+                var rank = line[0];
+                wordRanks[word] = rank;
+                trie.add(word);
+              }
+              wiktionary = wiktionary;
+          }
+      });
+  }
+
+  function keyup() {
+    var prefix = $$("inputBox").value;
+    $$("list").innerHTML = "";
+    if (prefix.length > 0) {
+      var words = trie.findWords(prefix);
+      words.sort(function(a, b) {
+        return wordRanks[b] - wordRanks[a];
+      })
+      for (var i = 0; i < Math.min(words.length, 10); i++) {
+        $$("list").innerHTML += "<li>" + words[i] + "</li>";
+      }
+    }
+    updateVisibility();
+  }
+
+  function keydown() {
+    updateVisibility();
+  }
+
+  function updateVisibility() {
+    $$("list").style.visibility = ($$("inputBox").value.length == 0) ? "hidden" : "";
+    $$("list").style.visibility = ($$("list").childNodes.length == 0) ? "hidden" : "";
+  }
+  readText();
+  $("#inputBox").keyup(function() { keyup(); });
+  $("#inputBox").keydown(function() { keydown(); })
+  $$("list").addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI'){
+      console.log(e.target);
+    }
+  }); 
+});
+
+
---
+++ trie.js
@@ -0,0 +1,82 @@
+/* Constructs a new Node setting the isWord flag
+   to false and an empty array of children */
+function Node() {
+  this.isWord = false;
+  this.children = [];
+}
+
+/* Constructs a new Trie with the root set
+   as a default empty Node */
+function Trie() {
+  this.root = new Node();
+}
+
+/* Inserts the given word into the Trie */
+Trie.prototype.add = function(word) {
+  traverse(this.root, word, true, false);
+}
+
+/* Returns true iff the given word is in the Trie */
+Trie.prototype.search = function(word) {
+  return traverse(this.root, word, false, true);
+}
+
+/* Returns true iff there is any word in the 
+   Trie that starts with the given prefix */
+Trie.prototype.startsWith = function(prefix) {
+  return traverse(this.root, prefix, false, false);
+}
+
+/* Returns an array of all words in the Trie
+   that start with the given prefix */
+Trie.prototype.findWords = function(prefix) {
+  var words = [];
+  var curr = this.root;
+  for (var i = 0; i < prefix.length; i++) {
+    var difference = prefix.charAt(i).charCodeAt(0) - 'a'.charCodeAt(0);
+    if (!curr.children[difference]) {
+      return words;
+    }
+    curr = curr.children[difference]
+  } 
+  findWords(curr, words, prefix);
+  return words;  
+}
+
+/* Accumulates the given array of words with all valid words
+   in the Trie that start from the given curr Node */
+var findWords = function(curr, words, currWord) {
+  if (!curr) {
+    return;
+  } else if (curr.isWord) {
+    words.push(currWord);
+  } else {
+    for (var i = 0; i < curr.children.length; i++) {
+      if (curr.children[i]) {
+        findWords(curr.children[i], words, currWord + String.fromCharCode('a'.charCodeAt(0)+ i));
+      }
+    }
+  }
+}
+
+/* Traverses the Trie with the given root Node
+   and adds Nodes or returns different boolean 
+   values dependent on the flags passed in */
+var traverse = function(root, word, create, search) {
+  var curr = root;
+  for (var i = 0; i < word.length; i++) {
+    var index = word.charAt(i).charCodeAt(0) - 'a'.charCodeAt(0);
+    if (!curr.children[index]) {
+      if (create) {
+        curr.children[index] = new Node();
+      } else {
+        return false;
+      }
+    }
+    curr = curr.children[index];
+  }
+  if (create) {
+    curr.isWord = true;
+  }
+  return (search) ? curr.isWord : true;
+}
